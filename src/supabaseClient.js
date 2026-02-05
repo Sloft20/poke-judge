0defd8a2ac7dc08e8cd3800bb065ec2cc9e3e59a
Sloft20 <thiagoelson10@gmail.com>
2026-02-05 15:17:20 -0300

    Fix: Importando cliente do supabase no Ap

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'; // Precisa desta importação também!
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey); //
+// Usamos import.meta.env para o Vite reconhecer as variáveis da Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
